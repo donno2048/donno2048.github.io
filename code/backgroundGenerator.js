0f387f9b6d5bc90fbff2505a5c5182e511dcaf65
Elisha Hollander <just4now666666@gmail.com>
2021-03-31 19:54:20 +0300

    Update backgroundGenerator.js

--- code/backgroundGenerator.js
+++ code/backgroundGenerator.js
@@ -1,8 +1,3 @@
-/**
-	Generates the backgrounds for a level.
-	Code by Rob Kleffner, 2011
-*/
-
 Mario.BackgroundGenerator = function(width, height, distant, type) {
     this.Width = width;
     this.Height = height;
@@ -37,10 +32,10 @@ Mario.BackgroundGenerator.prototype = {
     GenerateOverground: function(level) {
         var range = this.Distant ? 4 : 6;
         var offs = this.Distant ? 2 : 1;
-        var oh = Math.floor(Math.random() * range) + offs;
+        var oh;
         var h = Math.floor(Math.random() * range) + offs;
         
-        var x = 0, y = 0, h0 = 0, h1 = 0, s = 2;
+        var x = 0, y = 0, h0 = 0, h1 = 0, s;
         for (x = 0; x < this.Width; x++) {
             oh = h;
             while (oh === h) {
@@ -50,7 +45,6 @@ Mario.BackgroundGenerator.prototype = {
             for (y = 0; y < this.Height; y++) {
                 h0 = (oh < h) ? oh : h;
                 h1 = (oh < h) ? h : oh;
-                s = 2;
                 if (y < h0) {
                     if (this.Distant){
                         s = 2;
